c129b1e19b4df89fe11328517fc8d0ddf29b6d75
Ubuntu <ubuntu@ip-172-31-29-244.us-east-2.compute.internal>
2019-11-07 14:42:48 +0000

    git ignore add

--- config/database.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    'connection': {
-        'host': 'localhost',
-        'user': 'root',
-        'password': 'root'
-    },
-	'database': 'soon',
-    'users_table': 'users'
-};

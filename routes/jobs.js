820b2deaad9e053b88a52ad24ecd3950f80427b6
jungwoochang <woochang4862@gmail.com>
2020-12-13 16:56:09 +0900

    update jobs

--- routes/jobs.js
+++ routes/jobs.js
@@ -7,6 +7,11 @@ const dbconfig = require('../config/database');
 const connection = mysql.createConnection(dbconfig.connection);
 const async = require("async");
 const request = require('request');
+var admin = require('firebase-admin');
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+  databaseURL: 'https://soon-79c2e.firebaseio.com'
+});
 
 connection.query('USE ' + dbconfig.database);
 
@@ -14,13 +19,13 @@ const _api_key = 'dacdeb969b934abef7e5002b69d6c9ae';
 const _url = 'https://api.themoviedb.org/3';
 
 Date.prototype.yyyymmdd = function () {
-    var mm = this.getMonth() + 1;
-    var dd = this.getDate();
+  var mm = this.getMonth() + 1;
+  var dd = this.getDate();
 
-    return [this.getFullYear(),
-    (mm > 9 ? '' : '0') + mm,
-    (dd > 9 ? '' : '0') + dd
-    ].join('-');
+  return [this.getFullYear(),
+  (mm > 9 ? '' : '0') + mm,
+  (dd > 9 ? '' : '0') + dd
+  ].join('-');
 };
 
 cron.schedule('* * * * *', function () {
@@ -89,22 +94,22 @@ cron.schedule('* * * * *', function () {
             console.log(hasDiff(tmp, movie_id_array));
             if (hasDiff(tmp, movie_id_array)) {
               var options = {
-                url : "https://api.themoviedb.org/3/company/"+row.company_id,
-                qs:{
-                  api_key:_api_key
+                url: "https://api.themoviedb.org/3/company/" + row.company_id,
+                qs: {
+                  api_key: _api_key
                 }
               }
-              request(options, function (err,response,body){
-                if(err) throw err;
+              request(options, function (err, response, body) {
+                if (err) throw err;
                 var message = {
                   to: '/topics/' + row.company_id,
-  
+
                   notification: {
                     title: 'Soon',
-                    body: JSON.parse(body).name+"에 새로운 영화가 등록되었습니다."
+                    body: JSON.parse(body).name + "에 새로운 영화가 등록되었습니다."
                   }
                 };
-  
+
                 fcm.send(message, function (err, response) {
                   if (err) {
                     console.log("Something has gone wrong!");
@@ -134,6 +139,87 @@ cron.schedule('* * * * *', function () {
       if (err) throw err;
       console.log("Complete!");
     });
+  });
+}).start();
+
+cron.schedule('* * * * *', function () {
+  connection.query('SELECT MIN(id), token FROM company_alarm GROUP BY token', function (err, rows) {
+    var i = 0;
+    async.whilst(
+      function () {
+        return i < rows.length;
+      },
+      function (next) {
+        const row = rows[i];
+        console.log(row.token);
+        var options = {
+          url: "https://iid.googleapis.com/iid/info/" + row.token,
+          headers: {
+            'Authorization': 'Key=' + serverKey
+          },
+          qs: {
+            details: true
+          }
+        };
+
+        request(options, function (err, response, body) {
+          var body = JSON.parse(body);
+          if (body.error && body.error == 'InvalidToken') {
+            // Invalid Token => Need to be Reset
+            connection.query('SELECT company_id FROM ' + dbconfig.company_alarm_table + ' WHERE token = ?', [row.token], function (err, rows) {
+              if (err) console.log('Error select with token:', err);
+              var j = 0;
+              async.whilst(
+                function () {
+                  return j < rows.length;
+                },
+                function (next) {
+                  var topic = rows[j].company_id;
+                  admin.messaging().unsubscribeFromTopic(row.token, '/topics/' + topic)
+                    .then(function (response) {
+                      var deleteSql = 'DELETE FROM ' + dbconfig.company_alarm_table + ' WHERE token = ? AND company_id = ?';
+                      connection.query(deleteSql, [row.token, topic], function (err, result) {
+                        if (err) console.log(err);;
+                        var sql = 'SELECT COUNT(*) AS membersCount FROM ' + dbconfig.company_alarm_table + ' WHERE company_id = ?'
+                        connection.query(sql, [topic], function (err, row) {
+                          if (err) console.log(err);;
+                          if (row[0].membersCount <= 0) {
+                            //DROP TABLE
+                            var dropSql = "DROP TABLE IF EXISTS `" + topic + "`"
+                            connection.query(dropSql, function (err, result) {
+                              if (err) console.log('', err);
+                              j++;
+                              return next();
+                            });
+                          } else {
+                            j++;
+                            return next();
+                          }
+                        });
+                      });
+                    })
+                    .catch(function (error) {
+                      console.log('Error unsubscribing from topic:', error);
+                    });
+                },
+                function (err) {
+                  if (err) console.log('', err);
+                  i++;
+                  next();
+                });
+            });
+          } else { // Valid Token
+            i++;
+            next();
+          }
+        });
+      },
+      function (err) {
+        //totally done!
+        if (err) console.log(err);
+        console.log('Totally Done!');
+      }
+    );
   });
 }).start();
 
---
+++ scripts/test_search_invalid_token.js
@@ -0,0 +1,91 @@
+const mysql = require('mysql');
+const dbconfig = require('../config/database');
+const connection = mysql.createConnection(dbconfig.connection);
+connection.query('USE ' + dbconfig.database);
+const request = require('request');
+const serverKey = 'AAAAI0G_Y0Q:APA91bGhn2kP760NOiIen0omFbsXL5Y2rjo8xXMruA3NOB7ejNBWIFW0QvUfVejexO_ZBMppGcoBEFK_1rWeuI2SD2pWWXCkxmPToCh8usIQx9W25krGRGekcbo2WOS27YmazEWQQNgF';
+const async = require("async");
+var admin = require('firebase-admin');
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+  databaseURL: 'https://soon-79c2e.firebaseio.com'
+});
+
+connection.query('SELECT MIN(id), token FROM company_alarm GROUP BY token', function (err, rows) {
+    var i = 0;
+    async.whilst(
+        function () {
+            return i < rows.length;
+        },
+        function (next) {
+            const row = rows[i];
+            console.log(row.token);
+            var options = {
+                url: "https://iid.googleapis.com/iid/info/" + row.token,
+                headers: {
+                    'Authorization': 'Key=' + serverKey
+                },
+                qs: {
+                    details: true
+                }
+            };
+
+            request(options, function (err, response, body) {
+                var body = JSON.parse(body);
+                if (body.error && body.error == 'InvalidToken') {
+                    // Invalid Token => Need to be Reset
+                    connection.query('SELECT company_id FROM ' + dbconfig.company_alarm_table + ' WHERE token = ?', [row.token], function (err, rows) {
+                        if (err) console.log('Error select with token:', err);
+                        var j = 0;
+                        async.whilst(
+                            function () {
+                                return j < rows.length;
+                            },
+                            function (next) {
+                                var topic = rows[j].company_id;
+                                admin.messaging().unsubscribeFromTopic(row.token, '/topics/' + topic)
+                                    .then(function (response) {
+                                        var deleteSql = 'DELETE FROM ' + dbconfig.company_alarm_table + ' WHERE token = ? AND company_id = ?';
+                                        connection.query(deleteSql, [row.token, topic], function (err, result) {
+                                            if (err) console.log(err);;
+                                            var sql = 'SELECT COUNT(*) AS membersCount FROM ' + dbconfig.company_alarm_table + ' WHERE company_id = ?'
+                                            connection.query(sql, [topic], function (err, row) {
+                                                if (err) console.log(err);;
+                                                if (row[0].membersCount <= 0) {
+                                                    //DROP TABLE
+                                                    var dropSql = "DROP TABLE IF EXISTS `" + topic + "`"
+                                                    connection.query(dropSql, function (err, result) {
+                                                        if (err) console.log('', err);
+                                                        j++;
+                                                        return next();
+                                                    });
+                                                } else {
+                                                    j++;
+                                                    return next();
+                                                }
+                                            });
+                                        });
+                                    })
+                                    .catch(function (error) {
+                                        console.log('Error unsubscribing from topic:', error);
+                                    });
+                            },
+                            function (err) {
+                                if (err) console.log('', err);
+                                i++;
+                                next();
+                            });
+                    });
+                } else { // Valid Token
+                    i++;
+                    next();
+                }
+            });
+        },
+        function (err) {
+            //totally done!
+            if (err) console.log(err);
+            console.log('Totally Done!');
+        }
+    );
+});

9a5fd3e705ea138b693f783d36f2d5ee8849f09e
woochang4862 <woochang4862@gmail.com>
2021-08-20 00:08:23 +0900

    add exception process in watch_providers routes

--- routes/movie.js
+++ routes/movie.js
@@ -296,7 +296,7 @@ router.get('/watch/providers', (req, res) => {
             }
         });
     } else {
-        return res.statusCode(400) //BAD REQUEST! : 올바르지 못한 리전 코드
+        return res.status(400) //BAD REQUEST! : 올바르지 못한 리전 코드
     }
 });
 

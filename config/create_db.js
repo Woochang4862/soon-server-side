5c77595c59878fd6f948c9d04830155e3dfc77db
woochang4862 <woochang4862@gmail.com>
2023-08-24 16:59:18 +0900

    modify Dockerfile to init database

--- config/create_db.js
+++ config/create_db.js
@@ -1,19 +1,24 @@
 const mysql = require('mysql2');
 const dbconfig = require('./database');
 
-const connection = mysql.createConnection(dbconfig.connection);
+function createTable() {
+        const connection = mysql.createConnection(dbconfig.connection);
+        connection.query("CREATE DATABASE IF NOT EXISTS " + dbconfig.database + ";");
 
-connection.query("CREATE DATABASE IF NOT EXISTS " + dbconfig.database + ";");
+        connection.query("USE " + dbconfig.database);
 
-connection.query("USE " + dbconfig.database);
+        connection.query('CREATE TABLE IF NOT EXISTS ' + dbconfig.company_alarm_table + '(token VARCHAR(300) NOT NULL, company_id INT UNSIGNED NOT NULL, PRIMARY KEY(token, company_id));', function (err, row){
+                if (err){
+                        console.log(err);
+                }
+                else{
+                        console.log(row);
+                }
+        });  
+        connection.end();
+}
 
-connection.query('CREATE TABLE IF NOT EXISTS ' + dbconfig.company_alarm_table + '(token VARCHAR(300) NOT NULL, company_id INT UNSIGNED NOT NULL, PRIMARY KEY(token, company_id));', function (err, row){
-        if (err){
-                console.log(err);
-        }
-        else{
-                console.log(row);
-        }
-});
+module.exports = createTable;
 
-connection.end();
+
+

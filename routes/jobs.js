71e9e84a7821cd34cd3f2c02faaa0123c75a4ec1
woochang4862 <woochang4862@gmail.com>
2024-05-25 13:51:51 +0900

    delete unnecessary log like response data, sql query response ...

--- routes/jobs.js
+++ routes/jobs.js
@@ -41,10 +41,10 @@ const checkDifferenceOfCompanyTable = async function (connection) {
   let response;
   try {
     response = await connection.query("SELECT * FROM " + dbconfig.company_alarm_table);
-    console.log("response of select from company alarm table: " + JSON.stringify(response));
+    console.log("response of select from company alarm table: " + JSON.stringify(response[0]));
 
     response = await connection.query("SELECT table_name FROM Information_schema.tables WHERE table_schema = '" + dbconfig.connection.database + "' AND table_name <> '" + dbconfig.company_alarm_table + "'");
-    console.log("response of select all company table: " + JSON.stringify(response)); // [[{"TABLE_NAME":"420"}],[...]]
+    console.log("response of select all company table: " + JSON.stringify(response[0])); // [[{"TABLE_NAME":"420"}],[...]]
     let [rows] = response;
     console.log(rows);
     for (let { TABLE_NAME } of rows) { // 대소문자 중요 table_name (x)
@@ -82,7 +82,7 @@ const checkDifferenceOfCompanyTable = async function (connection) {
       console.log('current movie_id_array from api : ' + JSON.stringify(movie_id_array_from_api));
 
       response = await connection.query("SELECT movie_id FROM `" + TABLE_NAME + "`");
-      console.log("response of select from `"+TABLE_NAME+"`: " + JSON.stringify(response)); //[[{"movie_id":533535},{"movie_id":609681},{"movie_id":617126},{"movie_id":617127},{"movie_id":822119},{"movie_id":986056},{"movie_id":1003596},{"movie_id":1003598},{"movie_id":1165487},{"movie_id":1165500}],[{"_buf":{"type":"Buffer","data":[1,0,0,1,1,52,0,0,2,3,100,101,102,8,97,108,97,114,109,95,100,98,3,52,50,48,3,52,50,48,8,109,111,118,105,101,95,105,100,8,109,111,118,105,101,95,105,100,12,63,0,10,0,0,0,3,35,80,0,0,0,5,0,0,3,254,0,0,3,0,7,0,0,4,6,53,51,51,53,51,53,7,0,0,5,6,54,48,57,54,56,49,7,0,0,6,6,54,49,55,49,50,54,7,0,0,7,6,54,49,55,49,50,55,7,0,0,8,6,56,50,50,49,49,57,7,0,0,9,6,57,56,54,48,53,54,8,0,0,10,7,49,48,48,51,53,57,54,8,0,0,11,7,49,48,48,51,53,57,56,8,0,0,12,7,49,49,54,53,52,56,55,8,0,0,13,7,49,49,54,53,53,48,48,5,0,0,14,254,0,0,3,0]},"_clientEncoding":"utf8","_catalogLength":3,"_catalogStart":10,"_schemaLength":8,"_schemaStart":14,"_tableLength":3,"_tableStart":23,"_orgTableLength":3,"_orgTableStart":27,"_orgNameLength":8,"_orgNameStart":40,"characterSet":63,"encoding":"binary","name":"movie_id","columnLength":10,"columnType":3,"type":3,"flags":20515,"decimals":0}]]
+      console.log("response of select from `"+TABLE_NAME+"`: " + JSON.stringify(response[0])); //[[{"movie_id":533535},{"movie_id":609681},{"movie_id":617126},{"movie_id":617127},{"movie_id":822119},{"movie_id":986056},{"movie_id":1003596},{"movie_id":1003598},{"movie_id":1165487},{"movie_id":1165500}],[{"_buf":{"type":"Buffer","data":[1,0,0,1,1,52,0,0,2,3,100,101,102,8,97,108,97,114,109,95,100,98,3,52,50,48,3,52,50,48,8,109,111,118,105,101,95,105,100,8,109,111,118,105,101,95,105,100,12,63,0,10,0,0,0,3,35,80,0,0,0,5,0,0,3,254,0,0,3,0,7,0,0,4,6,53,51,51,53,51,53,7,0,0,5,6,54,48,57,54,56,49,7,0,0,6,6,54,49,55,49,50,54,7,0,0,7,6,54,49,55,49,50,55,7,0,0,8,6,56,50,50,49,49,57,7,0,0,9,6,57,56,54,48,53,54,8,0,0,10,7,49,48,48,51,53,57,54,8,0,0,11,7,49,48,48,51,53,57,56,8,0,0,12,7,49,49,54,53,52,56,55,8,0,0,13,7,49,49,54,53,53,48,48,5,0,0,14,254,0,0,3,0]},"_clientEncoding":"utf8","_catalogLength":3,"_catalogStart":10,"_schemaLength":8,"_schemaStart":14,"_tableLength":3,"_tableStart":23,"_orgTableLength":3,"_orgTableStart":27,"_orgNameLength":8,"_orgNameStart":40,"characterSet":63,"encoding":"binary","name":"movie_id","columnLength":10,"columnType":3,"type":3,"flags":20515,"decimals":0}]]
       let movie_id_array_from_table = response[0].map(function (e) {
         return e.movie_id
       });
@@ -141,7 +141,7 @@ const unsubscribeInvalidToken = async function (connection) {
   let response;
   try {
     response = await connection.query('SELECT token FROM ' + dbconfig.company_alarm_table + ' GROUP BY token'); // token 을 unique 하게 뽑아냄
-    console.log("response: " + JSON.stringify(response));
+    console.log("response: " + JSON.stringify(response[0]));
 
     for (let {token} of response[0]){
       response = await fetch("https://iid.googleapis.com/iid/info/" + token+"?" + 
@@ -164,7 +164,7 @@ const unsubscribeInvalidToken = async function (connection) {
          * company_alarm_table 에서 token 행 삭제
          */
         response = await connection.query('SELECT company_id FROM ' + dbconfig.company_alarm_table + ' WHERE token="'+token+'"');
-        console.log("response: "+response);
+        console.log("response of select company_id from company alarm table where token = "+token+" : "+response[0]);
         
         for (let {company_id} of response[0]) {
           response = await connection.query("DROP TABLE IF EXISTS `" + company_id + "`");
@@ -249,21 +249,18 @@ function getDiff(a1, a2) {
 }
 
 
-cron.schedule('0 0 1 * *', function () {
-  var message = {
-    to: '/topics/all',
-
-    notification: {
+cron.schedule('0 0 1 * *', async function () {
+  let message = {
+    topic: 'all',
+    data: {
       title: 'Soon',
-      body: '이번달에 무슨 영화가 개봉하는지 확인하세요'
+      body: '이번달에 무슨 영화가 개봉하는지 확인해보세요!'
     }
   };
-
-  fcm.send(message, function (err, response) {
-    if (err) {
-      console.log("Something has gone wrong!");
-    } else {
-      console.log("Successfully sent with response: ", response);
-    }
-  });
+  try {
+    response = await admin.messaging().send(message);
+    console.log("Successfully sent with response: ", response);
+  } catch (error) {
+    console.log("Something has gone wrong!\n"+err);
+  }
 }).start();

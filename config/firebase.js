ef79717bb396b4c5c1f6ac654a2247b6befe8867
정우창 <woochang4862@gmail.com>
2025-01-19 22:55:40 +0900

    FCM 인증 방식 변경

--- config/firebase.js
+++ config/firebase.js
@@ -3,7 +3,7 @@ config();
 
 export default function getAccessToken() {
     return new Promise(async function (resolve, reject) {        
-        const key = await import('../public/service-account.json', { assert: { type: 'json' } });
+        const key = await import('../public/soon-79c2e-firebase-adminsdk-h7o9r-dc2b66a1c8.json', { assert: { type: 'json' } });
         const jwtClient = new google.auth.JWT(
             key.client_email,
             null,
